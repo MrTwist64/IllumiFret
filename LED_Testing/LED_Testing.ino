964d0887e82188c1a0186b4469531270407117e0
Aaron Fleming <63603135+MrTwist64@users.noreply.github.com>
2023-04-17 13:52:47 -0700

    Experimentation

--- LED_Testing/LED_Testing.ino
+++ LED_Testing/LED_Testing.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
-#define NUM_LEDS 24
+#define NUM_LEDS 138
 #define DATA_PIN D0
-#define BRIGHTNESS 24
+#define BRIGHTNESS 10
 
 Adafruit_NeoPixel pixels(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -16,20 +16,22 @@ void loop() {
   for(int i = 0; i < NUM_LEDS + 1; i++)
   {
     for(int j = 0; j < i; j++)
-      pixels.setPixelColor(j, pixels.Color(BRIGHTNESS-j, j, 0));
+      pixels.setPixelColor(j, pixels.Color(30, 30, 30));
     pixels.show();
-    delay(200);
+    delay(10);
   }
-  //delay(100);
+  delay(100000);
   
-  /*
-  pixels.setPixelColor(1, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
-  pixels.setPixelColor(2, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
-  pixels.setPixelColor(15, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
-  pixels.setPixelColor(17, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
-  pixels.setPixelColor(22, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
-  pixels.show();
-  */
+  
+  // pixels.setPixelColor(1, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  // pixels.setPixelColor(2, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  // pixels.setPixelColor(15, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  // pixels.setPixelColor(17, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  // pixels.setPixelColor(22, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  // pixels.show();
+
+  // pixels.setPixelColor(23, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  
   
   pixels.clear();
 }
