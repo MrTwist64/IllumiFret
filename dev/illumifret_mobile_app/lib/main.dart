a0cdd8ad0621610cfc0f310912563b7f3149e059
Aaron Fleming <63603135+MrTwist64@users.noreply.github.com>
2023-05-12 17:59:50 -0700

    Added new scales and chords

--- dev/illumifret_mobile_app/lib/main.dart
+++ dev/illumifret_mobile_app/lib/main.dart
@@ -280,24 +280,64 @@ class MainMenu extends StatelessWidget {
 class ChordList extends DisplayStrList {
   final String barTitle = "Chords";
   final List<List<String>> itemList = [
-    ["A.txt", "A"],
-    ["ASharp.txt", "A# / Bb"],
-    ["B.txt", "B"],
-    ["C.txt", "C"],
-    ["CSharp.txt", "C# / Db"],
-    ["D.txt", "D"],
-    ["DSharp.txt", "D# / Eb"],
-    ["E.txt", "E"],
+    ["Amaj_1.txt", "A Maj - 1"],
+    ["Amaj_2.txt", "A Maj - 2"],
+    ["Amaj_3.txt", "A Maj - 3"],
+    ["Amin_1.txt", "A Min - 1"],
+    ["Amin_2.txt", "A Min - 2"],
+    ["Amin_3.txt", "A Min - 3"],
+
+    ["Bmaj_1.txt", "B Maj - 1"],
+    ["Bmaj_2.txt", "B Maj - 2"],
+    ["Bmaj_3.txt", "B Maj - 3"],
+    ["Bmin_1.txt", "B Min - 1"],
+    ["Bmin_2.txt", "B Min - 2"],
+
+    ["Cmaj_1.txt", "C Maj - 1"],
+    ["Cmaj_2.txt", "C Maj - 2"],
+    ["Cmaj_3.txt", "C Maj - 3"],
+    ["Cmin_1.txt", "C Min - 1"],
+    ["Cmin_2.txt", "C Min - 2"],
+
+    ["Dmaj_1.txt", "D Maj - 1"],
+    ["Dmaj_2.txt", "D Maj - 2"],
+    ["Dmaj_3.txt", "D Maj - 3"],
+    ["Dmin_1.txt", "D Min - 1"],
+    ["Dmin_2.txt", "D Min - 2"],
+
+    ["Emaj_1.txt", "E Maj - 1"],
+    ["Emaj_2.txt", "E Maj - 2"],
+    ["Emaj_3.txt", "E Maj - 3"],
+    ["Emin_1.txt", "E Min - 1"],
+    ["Emin_2.txt", "E Min - 2"],
+    ["Emin_3.txt", "E Min - 3"],
+
+    ["Fmaj_1.txt", "F Maj - 1"],
+    ["Fmaj_2.txt", "F Maj - 2"],
+    ["Fmaj_3.txt", "F Maj - 3"],
+    ["Fmin_1.txt", "F Min - 1"],
+    ["Fmin_2.txt", "F Min - 2"],
+
+    ["Gmaj_1.txt", "G Maj - 1"],
+    ["Gmaj_2.txt", "G Maj - 2"],
+    ["Gmaj_3.txt", "G Maj - 3"],
+    ["Gmin_1.txt", "G Min - 1"],
+    ["Gmin_2.txt", "G Min - 2"],
   ];
 }
 
 class ScaleList extends DisplayStrList {
   final String barTitle = "Scales";
   final List<List<String>> itemList = [
-    [".txt", "I Pentatonic"],
-    [".txt", "II Ionian"],
-    [".txt", "III Minor Pentatonic"],
-    [".txt", "IV Minor Ionian"],
+    ["Amaj_P.txt", "Amaj_P"],
+    ["Amin_P.txt", "Amin_P"],
+    ["Bmaj_P.txt", "Bmaj_P"],
+    ["Bmin_P.txt", "Bmin_P"],
+    ["Cmaj_P.txt", "Cmaj_P"],
+    ["Dmaj_P.txt", "Dmaj_P"],
+    ["Dmin_P.txt", "Dmin_P"],
+    ["Emaj_P.txt", "Emaj_P"],
+    ["Fmaj_P.txt", "Fmaj_P"],
   ];
 }
 
